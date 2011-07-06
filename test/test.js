e813f84c8de44374c4b1a06e630fad44b67ed602
Aseem Kishore <aseem.kishore@gmail.com>
2011-07-06 02:13:44 -0400

    Add test for script ordering. Uses JSDOM!

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 app = require('./app');
 assert = require('assert');
+jsdom = require('jsdom');
 
 function testRoot(exp) {
     assert.response(app, {
@@ -21,5 +22,34 @@ exports["test basic layout ordering"] = function () {
         var i2 = body.indexOf('<h2>Parent</h2>');
         var i3 = body.indexOf('<h3>Child</h3>');
         assert.ok(i1 < i2 && i2 < i3);
+    });
+};
+
+exports["test script ordering"] = function () {
+    testRoot(function (res) {
+        // between views, parent scripts should come before child scripts,
+        // but within a view, scripts should be in the original order.
+        jsdom.env(res.body, function (err, window) {
+            if (err) {
+                assert.fail('Unexpected: could not parse test site DOM');
+            }
+
+            scripts = window.document.getElementsByTagName('script');
+
+            // convert to node's ES5 array and map <script> tags to srcs:
+            scripts = Array.prototype.slice.call(scripts).map(function (script) {
+                return script.src;
+            })
+
+            // the srcs should match this order exactly:
+            assert.deepEqual(scripts, [
+                'master1.js',
+                'master2.js',
+                'parent1.js',
+                'parent2.js',
+                'child1.js',
+                'child2.js',
+            ]);
+        });
     });
 };
