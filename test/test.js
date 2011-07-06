817a08a5c4e7cd72b14577e70b673c7406ed78a9
Aseem Kishore <aseem.kishore@gmail.com>
2011-07-06 02:01:45 -0400

    Add test case for basic layout ordering.

--- test/test.js
+++ test/test.js
@@ -13,3 +13,13 @@ exports["test basic not-broken-ness"] = function () {
         body: /Express\-Blocks Test Site/
     });
 };
+
+exports["test basic layout ordering"] = function () {
+    testRoot(function (res) {
+        var body = res.body;
+        var i1 = body.indexOf('<h1>Master</h1>');
+        var i2 = body.indexOf('<h2>Parent</h2>');
+        var i3 = body.indexOf('<h3>Child</h3>');
+        assert.ok(i1 < i2 && i2 < i3);
+    });
+};
