83234e20c65a53358bdcdd51a43c0d484b73c1dd
Aseem Kishore <aseem.kishore@gmail.com>
2011-10-09 23:18:00 -0700

    Tweak test server to reveal concurrency bug.

--- test/app.js
+++ test/app.js
@@ -11,7 +11,10 @@ app.set('view engine', 'html');
 app.register('.html', require('ejs'));
 
 app.get('/', function (req, res) {
-    res.render('child');
+    // create an artificial delay to test concurrency
+    setTimeout(function () {
+        res.render('child');
+    }, 1000);
 });
 
 // since this is a test server, don't listen -- just return the server.
